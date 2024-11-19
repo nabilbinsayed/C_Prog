2b788aa6bca5191881f5069f1323e1c2e90f6a0a
Nabil Bin Sayed <nabilprometheus@gmail.com>
2024-11-19 23:01:32 +0600

    18.11.24

---
+++ scanf_format_intro.c
@@ -0,0 +1,20 @@
+// Name: 
+// Date: 
+// Course/book: 
+// Section: 3.2
+// Purpose: 
+// Note/Highlight: 
+
+#include <stdio.h>
+int main (void)
+
+{
+  int i, j;
+  float x, y; 
+
+  scanf("%d %d%f%f", &i, &j, &x, &y);
+  printf("%d %d %g %g", i, j, x, y);
+
+ 
+  return 0; 
+}
