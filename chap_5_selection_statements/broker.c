b2841751e441f0ca8d75ef2b1f6d2155a2ececb4
Nabil Bin Sayed <nabilprometheus@gmail.com>
2024-12-06 01:44:19 +0600

    before 6.12.24

---
+++ chap_5_selection_statements/broker.c
@@ -0,0 +1,36 @@
+// Name: 
+// Date: 
+// Course/book: 
+// Section: 
+// Purpose: 
+
+    //   Calculating a Broker’s Commission
+    // When stocks are sold or purchased through a broker, the broker’s commission is
+    // often computed using a sliding scale that depends upon the value of the stocks
+    // traded. Let’s say that a broker charges the amounts shown in the following table:
+    // Transaction size Commission rate
+    // Under $2,500 $30 + 1.7%
+    // $2,500–$6,250 $56 + 0.66%
+    // $6,250–$20,000 $76 + 0.34%
+    // $20,000–$50,000 $100 + 0.22%
+    // $50,000–$500,000 $155 + 0.11%
+    // Over $500,000 $255 + 0.09%
+    // The minimum charge is $39. Our next program asks the user to enter the amount of
+    // the trade, then displays the amount of the commission:
+    // Enter value of trade: 30000
+    // Commission: $166.00
+    // The heart of the program is a cascaded if statement that determines which range
+    // the trade falls into.
+
+
+// Note/Highlight: 
+
+#include <stdio.h>
+int main (void)
+
+{
+  
+
+
+  return 0; 
+}
---
+++ chap_5_selection_statements/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#define PI acos(-1)
+
+int main(void)
+{
+  printf("%.20Lf\n", PI);
+
+  return 0; 
+}
