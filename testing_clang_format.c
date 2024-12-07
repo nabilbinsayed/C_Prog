7ea9828674527bfc4a56b6c892edfbd111cf54fe
Nabil Bin Sayed <nabilprometheus@gmail.com>
2024-12-07 22:04:33 +0600

    not sure who's left out

--- testing_clang_format.c
+++ testing_clang_format.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
-int main(void) {
+int main(void)
+{
   int i, j;
 
-  i = 1, j = 2;
+  i = 2, j = 1;
   int k = 2 * i + j - i;
-  if (i > j) {
-    printf("%d is greater than %d\n", i, j);
-  } else if (i < j) {
-    printf("%d is greater than %d\n", j, i);
-  } else {
-    printf("both of them are same\n");
+  // if (i > j) {
+  //   printf("%d is greater than %d\n", i, j);
+  // }
+  // else if (i < j) {
+  //   printf("%d is greater than %d\n", j, i);
+  // }
+  // else {
+  //   printf("both of them are same\n");
+  // }
+
+  if (!(i < j)) {
+    if (i == j) {
+      printf("i=j\n");
+    }
+    else {
+      printf("i>j\n");
+    }
+  }
+  else {
+    printf("i<j\n");
   }
 
   return 0;
