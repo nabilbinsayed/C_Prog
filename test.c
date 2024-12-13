13336d973dc0273774a456c4c559097290e9cf8d
nabilbinsayed <nabilprometheus@gmail.com>
2024-12-13 20:45:30 +0600

    test2

--- test.c
+++ test.c
@@ -2,6 +2,6 @@
 int main(void)
 {
   printf("Here's to a new beginning, cheers!\n"); 
-
+  printf("Again, just testing out the \"git push instead of git push origin main thingy\"\n"); 
   return 0; 
 }
