eb3f244bcc1689c53974350e1f5f90c0024792e7
nabilbinsayed <nabilprometheus@gmail.com>
2024-12-16 18:02:11 +0600

    NEW PC!

--- test.c
+++ test.c
@@ -3,5 +3,8 @@ int main(void)
 {
   printf("Here's to a new beginning, cheers!\n"); 
   printf("Again, just testing out the \"git push instead of git push origin main thingy\"\n"); 
+
+  printf("From a new PC!\n Let's test this origin main thing again!\n"); 
+  
   return 0; 
 }
